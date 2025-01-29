6834318a87e35cb003f653fe6c0628255a1e863d
dpbd-2004 <142929884+dpbd-2004@users.noreply.github.com>
2025-01-29 21:46:44 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "darren@gmail.com"
+var accountPassword = "12345"
+accountCity = "bangalore"
+
+//accountId = 2   //not allowed
+
+accountEmail = "hc@gmail.com"
+accountPassword = "12345566"
+accountCity = "jaipur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
