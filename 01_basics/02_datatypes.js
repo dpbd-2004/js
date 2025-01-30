5c3c10fed0158a3726daff914d3d78c015db1774
dpbd-2004 <142929884+dpbd-2004@users.noreply.github.com>
2025-01-30 10:52:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict";  //treat all js code as newer version
+
+// alert(3+3)  we are using nodejs, not browser
+
+let name = "darren"
+let age = 20
+let isloggedIn = false
+
+console.log(typeof"darren");
