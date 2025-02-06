df8d1708b6b4852d92cc0319315a4e85087a2a2b
dpbd-2004 <142929884+dpbd-2004@users.noreply.github.com>
2025-02-06 18:10:10 +0000

    datatype summary

---
+++ 01_basics/datatypes2.js
@@ -0,0 +1,30 @@
+//primitive
+ const score = 100
+ const scoreValue = 100.3
+ const isLoggedIn = false
+ const outsideTemp = null
+ let userEmail;
+
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+
+ console.log(id === anotherId)
+
+ const bigNumber = 343536546757567554n
+
+ //non primitive
+
+ const heros = ["thor","ironman","hulk"]
+ 
+ let myObj = {
+    name: "darren",
+    age: 20,
+ }
+
+ const myFunction = function(){
+    console.log("hello world")
+ }
+
+console.log(typeof bigNumber)
+console.log(typeof myFunction)
+
