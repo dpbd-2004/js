b46ec5f777bbd6a9f82f31e889ed25906832ad7c
dpbd-2004 <142929884+dpbd-2004@users.noreply.github.com>
2025-02-18 14:19:24 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,3 +31,38 @@ function loginUserMessage(username){
 
 console.log(loginUserMessage("darren"))
 console.log(loginUserMessage())
+
+
+function calculateCartPrice(...num1) {
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500,2000))
+
+
+const user  = {
+    username : "darren",
+    price : 199
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price ${anyobject.price}`)
+
+}
+
+handleObject(user)
+
+handleObject({
+    username:"sam",
+    price: 200
+})
+
+const myNewArray = [200,400,500,600]
+
+function returnSecondValue(getArray){
+    return getArray[1];
+
+}
+
+console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,400,500,600]))
