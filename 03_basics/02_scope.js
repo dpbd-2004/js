b42fe7b1078c30c6288898bf24d1f423735c3346
dpbd-2004 <142929884+dpbd-2004@users.noreply.github.com>
2025-02-19 21:27:22 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,16 @@
+
+var c = 300
+let a = 400
+
+console.log(a)
+console.log(c);
+
+if (true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+console.log(a);
+// console.log(b);
+console.log(c);
